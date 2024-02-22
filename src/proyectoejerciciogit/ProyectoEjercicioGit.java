3797fd9630e7710e77e9281c8ceb3db0d71c34c2
dam <dam@203P22>
2024-02-22 09:19:35 +0100

    2 com

--- src/proyectoejerciciogit/ProyectoEjercicioGit.java
+++ src/proyectoejerciciogit/ProyectoEjercicioGit.java
@@ -16,7 +16,7 @@ public class ProyectoEjercicioGit {
     public static void main(String[] args) {
         
         System.out.println("EOEOEOEOEOEOEEO sergio");
-        
+        System.out.println("fmgktmpokrtmhn");
     }
     
 }
