2397c594419b1c6b6abbea3a1a6bcbd406c8906d
dam <dam@203P21>
2024-02-22 09:03:54 +0100

    1v sergio

---
+++ src/proyectoejerciciogit/ProyectoEjercicioGit.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectoejerciciogit;
+
+/**
+ *
+ * @author dam
+ */
+public class ProyectoEjercicioGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("EOEOEOEOEOEOEEO sergio");
+        
+    }
+    
+}
